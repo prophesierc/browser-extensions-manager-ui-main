422313afb0ea28b09ce6a094861fdd3ccb724622
ProphesierC <bwknowles98@gmail.com>
2025-04-07 10:58:33 -0400

    Pre setup for Project Developement

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,7 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <HomeView/>
 </template>
+
+<script setup lang="ts">
+  import HomeView from '@/views/HomeView.vue';
+</script>
