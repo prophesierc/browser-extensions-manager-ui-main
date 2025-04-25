6b727acfc85e87da9861cb37b24b50d6842acb74
ProphesierC <bwknowles98@gmail.com>
2025-04-25 13:44:21 -0400

    update 8 files and delete 1 file

--- src/Components/ExtensionsBar.vue
+++ src/Components/ExtensionsBar.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="bg-white h-auto p-2 flex flex-col text-center rounded-xl shadow-default">
       <span class="flex flex-row items-center justify-between">
-          <img src="@/assets/images/logo.svg"/>
-          <img class="bg-Neutral-100 p-3 mr-1 rounded-xl " src="@/assets/images/icon-moon.svg"/>
+          <img src="/src/assets/images/logo.svg">
+          <img class="bg-Neutral-100 p-3 mr-1 rounded-xl " src="/src/assets/images/icon-moon.svg"/>
       </span>
   </div>
 </template>
